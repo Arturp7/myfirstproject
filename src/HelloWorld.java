ce688ae0cdffd1dec8affa65590d8c3b624bc420
Artur Przyjemski <spinezpl@gmail.com>
2019-04-26 19:50:06 +0200

    Dodanie pliku kolejnej linii porawka 3 new

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
         System.out.println("Hello World2");
         System.out.println("Hello World1");
         System.out.println("Wita Nas nowy wp");
-        System.out.println();
+        System.out.printetst);
 
     }
     }
